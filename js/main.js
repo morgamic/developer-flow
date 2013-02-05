2054f2746ad9b3fa9009942cafd79dd73579d620
morgamic <morgamic@gmail.com>
2013-02-05 11:14:32 -0800

    adding tooltips and some minor edits

---
+++ js/main.js
@@ -0,0 +1,5 @@
+$(document).ready(function () {
+    $(".done").tooltip({'title':'This is in production.  It needs polish but has shipped.', 'placement':'right'});
+    $(".started").tooltip({'title':'This has been started but not launched.  Click through to learn more.', 'placement':'right'});
+    $(".fire").tooltip({'title':'This is something that needs love and feeding.  We know it\'s important but haven\'t gotten to it yet.', 'placement':'right'});
+});
