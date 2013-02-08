7ad762f218d26a0fef0ab90cd3d2bb35990eb8c1
morgamic <morgamic@gmail.com>
2013-02-08 12:03:23 -0800

    updates and stuff

--- js/main.js
+++ js/main.js
@@ -2,4 +2,6 @@ $(document).ready(function () {
     $(".done").tooltip({'title':'This is in production.  It needs polish but has shipped.', 'placement':'right'});
     $(".started").tooltip({'title':'This has been started but not launched.  Click through to learn more.', 'placement':'right'});
     $(".fire").tooltip({'title':'This is something that needs love and feeding.  We know it\'s important but haven\'t gotten to it yet.', 'placement':'right'});
+    $(".icon-wrench").tooltip({'title':'Get the code and contribute!', 'placement':'right'});
+    $(".icon-book").tooltip({'title':'Read more about the requirements', 'placement':'right'});
 });
